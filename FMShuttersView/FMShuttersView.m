33f2ea65a3daf1bfd49ef7f31ce1055fb6a060cc
Andrea Ottolina <andrea.otto@tiscali.it>
2013-05-07 14:18:49 +0100

    New class FMShuttersView added to the project

---
+++ FMShuttersView/FMShuttersView.m
@@ -0,0 +1,31 @@
+//
+//  FMShuttersView.m
+//  FMShuttersViewDemo
+//
+//  Created by Andrea Ottolina on 07/05/2013.
+//  Copyright (c) 2013 Flubber Media. All rights reserved.
+//
+
+#import "FMShuttersView.h"
+
+@implementation FMShuttersView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
